6e355d291a1a20a7ff55df2cc57401d670519539
Kiriti-P <kiriti2003@gmail.com>
2023-01-28 22:53:23 -0600

    Added delete functionality

--- frontend/src/context/MedicationContext.js
+++ frontend/src/context/MedicationContext.js
@@ -1,32 +1,36 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useReducer } from "react";
 
-export const MedicationsContext = createContext()
+export const MedicationsContext = createContext();
 
 export const medicationsReducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_MEDICATIONS':
-            return {
-                medications: action.payload
-            }
-        case 'CREATE_MEDICATION':
-            return {
-                medications: [action.payload, ...state.medications]
-            }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case "SET_MEDICATIONS":
+      return {
+        medications: action.payload,
+      };
+    case "CREATE_MEDICATION":
+      return {
+        medications: [action.payload, ...state.medications],
+      };
+    case "DELETE_MEDICATION":
+      return {
+        medications: state.medications.filter(
+          (m) => m._id !== action.payload._id
+        ),
+      };
+    default:
+      return state;
+  }
+};
 
 export const MedicationsContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(medicationsReducer, {
-        medications: null
-    })
+  const [state, dispatch] = useReducer(medicationsReducer, {
+    medications: null,
+  });
 
-    
-
-    return (
-        <MedicationsContext.Provider value={{...state, dispatch}}>
-            { children }
-        </MedicationsContext.Provider>
-    )
-}
+  return (
+    <MedicationsContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </MedicationsContext.Provider>
+  );
+};
